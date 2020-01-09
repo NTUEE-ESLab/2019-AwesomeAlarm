d9130c257ef44bbb16626032edfe9ed1fae0bc5e
Jexus Chuang <b05901033@ntu.edu.tw>
2020-01-10 02:30:24 +0800

    add i2c bluetooth module

--- MainSystem.c
+++ MainSystem.c
@@ -2,15 +2,15 @@
 #include <TM1637Display.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
-#include<Servo.h>
+#include <Servo.h>
 
 
-SoftwareSerial I2CBT(5, 6); 
 int setting_hour = 0;
 int setting_min = 0;
 int setting_count = 0;
 int insize; 
 byte cmmd[20]; 
+int receivedmessage[10];
 
 // Module connection pins (Digital Pins)
 #define CLK 9
@@ -26,11 +26,12 @@ Servo servoVer;
 
 
 int buzzerPin=10;
-int trigPin = 12;                  //Trig Pin
-int echoPin = 11;                  //Echo Pin
-long duration, cm, inches;
+int trigPin = 5;                  //Trig Pin
+int echoPin = 6;                  //Echo Pin
+float duration, cm, inches;
 int my_dist[21];
 void ringTone(int pin) {
+  //return;
   for (int i=0; i<10; i++) { //repeat 10 times
     tone(pin, 1000);
     delay(50);
@@ -43,53 +44,48 @@ void ringTone(int pin) {
 
 int remain = 5;
 //請注意Arduino的TX要接藍牙模組的RX, 反之亦然
-const int start_pos = 120;
-const int end_pos = 80;
+const int start_pos = 130;
+const int end_pos = 90;
+const int x_step = 3;
+bool show_time = 0;
+
+int time_now = 0;
+void display_time(){
+  if (show_time){
+    time_now = setting_hour * 100 + setting_min;
+    display.showNumberDecEx(time_now, 0b11100000, true, 4, 0);
+  }
+}
+
+
 
 void distance()
 {
-//  for(int x = 50; x <= 150; x+=5){
-//    servoVer.write(x);
-//    delay(500);
-//    
-//    digitalWrite(trigPin, LOW);
-//    delayMicroseconds(5);
-//    digitalWrite(trigPin, HIGH);     // 給 Trig 高電位，持續 10微秒
-//    delayMicroseconds(10);
-//    digitalWrite(trigPin, LOW);
-//  
-//    pinMode(echoPin, INPUT);             // 讀取 echo 的電位
-//    duration = pulseIn(echoPin, HIGH);   // 收到高電位時的時間
-//   
-//    cm = (duration/2) / 29.1;         // 將時間換算成距離 cm 或 inch  
-//    inches = (duration/2) / 74; 
-//    Serial.print("Angle : "); 
-//    Serial.print(x); 
-//    Serial.print(", Distance : ");  
-//    Serial.print(inches);
-//    Serial.print("in,   ");
-//    Serial.print(cm);
-//    Serial.print("cm");
-//    Serial.println();
-//    
-//    delay(500);
-//  }
     int t = 0;
-    for(int x = start_pos; x >= end_pos; x-=5){
+    for(int x = start_pos; x >= end_pos; x-=x_step){
     servoVer.write(x);
-    delay(500);
-    
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(5);
-    digitalWrite(trigPin, HIGH);     // 給 Trig 高電位，持續 10微秒
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
+    while (1){
+      delay(200);
+      
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
+      digitalWrite(trigPin, HIGH);     // 給 Trig 高電位，持續 10微秒
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
   
-    pinMode(echoPin, INPUT);             // 讀取 echo 的電位
-    duration = pulseIn(echoPin, HIGH);   // 收到高電位時的時間
-   
-    cm = (duration/2) / 29.1;         // 將時間換算成距離 cm 或 inch  
-    inches = (duration/2) / 74; 
+      pinMode(echoPin, INPUT);             // 讀取 echo 的電位
+      duration = pulseIn(echoPin, HIGH);   // 收到高電位時的時間
+  
+  
+      Serial.print("Distance: ");
+      Serial.println(duration*0.034/2);
+  
+  
+     
+      cm = (duration/2) / 29.1;         // 將時間換算成距離 cm 或 inch  
+      inches = (duration/2) / 74; 
+      if (cm>10) break;
+    }
     Serial.print("Angle : "); 
     Serial.print(x); 
     Serial.print(", Distance : ");  
@@ -104,6 +100,7 @@ void distance()
     delay(500);
   }
 }
+      
 
 void find_lowest()
 {
@@ -112,10 +109,10 @@ void find_lowest()
     int maximum = 0;
     int maximum_t = 0;
     int max_angle = 0;
-    for(int x = start_pos; x >= end_pos; x-=5){
+    for(int x = start_pos; x >= end_pos; x-=x_step){
       servoVer.write(x);
       Serial.print(x); 
-      delay(500);
+      delay(1000);
       
       digitalWrite(trigPin, LOW);
       delayMicroseconds(5);
@@ -123,7 +120,7 @@ void find_lowest()
       delayMicroseconds(10);
       digitalWrite(trigPin, LOW);
     
-      pinMode(echoPin, INPUT);             // 讀取 echo 的電位
+//      pinMode(echoPin, INPUT); /            // 讀取 echo 的電位
       duration = pulseIn(echoPin, HIGH);   // 收到高電位時的時間
      
       cm = (duration/2) / 29.1;         // 將時間換算成距離 cm 或 inch  
@@ -161,30 +158,6 @@ void find_lowest()
     Serial.println();
 }  
 
-void check_setting_time(){
-  if ((insize=(I2CBT.available()))>0)
-  {  
-    Serial.print("input size = "); 
-    Serial.println(insize);  
-    for (int i=0; i<insize; i++) {
-      Serial.print(cmmd[i]=char(I2CBT.read()));
-      Serial.println(" ");  
-    }
-    if(insize==3){
-      setting_hour = (int)cmmd[0] - 48 + 10;
-      setting_count = (int)cmmd[1] - 48;
-      setting_min = (int)cmmd[2] -48;
-      Serial.println(setting_hour);Serial.println(setting_min); 
-    }
-    if(insize==4){
-      setting_hour = (int)cmmd[0] -48 + 10;
-      setting_count = (int)cmmd[1] - 48;
-      setting_min = ((int)cmmd[2] -48)*10 + (int)cmmd[3] - 48;
-      Serial.println(setting_hour);Serial.println(setting_min); 
-    }
-  } 
-}
-
 
 void setup()
 {
@@ -192,52 +165,72 @@ void setup()
   uint8_t data[] = { 0x0, 0x0, 0x0, 0x0 };
   pinMode(buzzerPin, OUTPUT);
   display.setSegments(data);
+  Wire.begin(9);
   pinMode(ledPin, OUTPUT);
   pinMode(laserPin, OUTPUT);
   pinMode(SERVO_PIN, OUTPUT);
   pinMode(CDSPin, INPUT);
   Serial.begin(9600);
-  I2CBT.begin(38400);
   pinMode(trigPin, OUTPUT);        //Define inputs and outputs 
   pinMode(echoPin, INPUT);
   servoVer.attach(SERVO_PIN, 544, 2400); 
   servoVer.write(start_pos);
+  
+  Wire.onReceive(receiveEvent);
 }
 
+void receiveEvent(int howmany) {
+  Serial.println(howmany);
+  for(int i=0;i<howmany; i++){
+    receivedmessage[i] = Wire.read(); // read one character from the I2C
+    Serial.println("received");
+    Serial.println(receivedmessage[i]);
+  }
+  setting_hour = receivedmessage[0];
+  setting_min =  receivedmessage[1];
+  setting_count = receivedmessage[2];
+}
 void loop()
 {
-  ringTone(buzzerPin);  
-
-  Serial.println("GALA1");
-  while(1){ 
-  Serial.println("GALA2");
-    check_setting_time();
-    if (setting_hour!=0) break;
+  
+  while(setting_count <= 1){
+//    Serial.println(setting_count);/
+    delay(500);
   }
-  Serial.println("GALA3");
+  Serial.println("jaha");
   distance();
   Serial.println("START");  
   digitalWrite(ledPin, LOW);
   digitalWrite(laserPin, LOW);
-
-  for(;remain>=0;remain--)
+  show_time = 0;
+  for(remain=setting_count;remain>=0;remain--)
   {
 //      display.showNumberDec(remain);
         display.showNumberDecEx(remain, 0b11100000, true, 4, 0);
       delay(1000);
   }
+  show_time = 1;
+  find_lowest();
+  Serial.println("RING");  
   digitalWrite(ledPin, HIGH);
   digitalWrite(laserPin, HIGH);
-  delay(1000);
-    Serial.println("RING");  
-  ringTone(buzzerPin);  
-  find_lowest();
+  delay(100); 
+  
+  ringTone(buzzerPin); 
+  ringTone(buzzerPin); 
+  ringTone(buzzerPin); 
   while(true){
     CDSVal = analogRead(CDSPin);
     Serial.println("READ");  
     Serial.println(CDSVal);  
     if (CDSVal<100){ break;}
-    delay(2000); //每2s讀取一次   
+    delay(1000); //每2s讀取一次   
+    display_time();
     
   }
+ setting_count = 0;
+display.showNumberDecEx(0, 0b11100000, true, 4, 0);
+  digitalWrite(ledPin, LOW);
+  digitalWrite(laserPin, LOW);
+ 
 }
